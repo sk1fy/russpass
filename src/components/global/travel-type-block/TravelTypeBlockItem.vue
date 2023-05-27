2d132ac6799481773a9de824843f634376637789
sk1fy <50354515+sk1fy@users.noreply.github.com>
2023-05-27 17:42:00 +0800

    adding dist

--- src/components/global/travel-type-block/TravelTypeBlockItem.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-<template>
-  <div class="travel-type-block__card__item d-flex">
-    Рекомендация
-    <span class="close-btn">
-      <img src="@/assets/img/close-btn.png" alt="close">
-    </span>
-  </div>
-</template>
